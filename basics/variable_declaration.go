90b3f8bbc7364523fcc1ecd02187762487b1da32
STEFFY THANKAM  WILSON <steffythankamwilson7@gmail.com>
2022-06-15 15:56:44 +0530

    Update variable_declaration.go

--- basics/variable_declaration.go
+++ basics/variable_declaration.go
@@ -18,19 +18,18 @@ func main() {
 	fmt.Printf("Type: %T Value: %v\n", id,id)
 	
 	t := 123      //Type Inferred will be int
-    u := "circle" //Type Inferred will be string
-    v := 5.6      //Type Inferred will be float64
-    w := true     //Type Inferred will be bool
-    x := 'a'      //Type Inferred will be rune
-    y := 3 + 5i   //Type Inferred will be complex128
-    z := sample{name: "test"}  //Type Inferred will be main.Sample
+	u := "circle" //Type Inferred will be string
+	v := 5.6      //Type Inferred will be float64  
+	w := true     //Type Inferred will be bool
+	x := 'a'      //Type Inferred will be rune   
+	y := 3 + 5i   //Type Inferred will be complex128   
+	z := sample{name: "test"}  //Type Inferred will be main.Sample
 	
 	fmt.Printf("Type: %T Value: %v\n", t, t)
-    fmt.Printf("Type: %T Value: %v\n", u, u)
-    fmt.Printf("Type: %T Value: %v\n", v, v)
-    fmt.Printf("Type: %T Value: %v\n", w, w)
-    fmt.Printf("Type: %T Value: %v\n", x, x)
-    fmt.Printf("Type: %T Value: %v\n", y, y)
-    fmt.Printf("Type: %T Value: %v\n", z, z)
+	fmt.Printf("Type: %T Value: %v\n", u, u)
+	fmt.Printf("Type: %T Value: %v\n", v, v)
+	fmt.Printf("Type: %T Value: %v\n", x, x)
+	fmt.Printf("Type: %T Value: %v\n", y, y)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
 
 }
