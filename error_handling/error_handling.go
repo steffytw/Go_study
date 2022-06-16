4769646fbfb8cb9284d20096f6c677fd447024f6
STEFFY THANKAM  WILSON <steffythankamwilson7@gmail.com>
2022-06-16 18:15:02 +0530

    ignore error

--- error_handling/error_handling.go
+++ error_handling/error_handling.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
+	"errors"
 )
 
 func main(){
-	file,err := os.Open("test.txt")
+	file1,err := os.Open("test.txt")
 	if err != nil{
 		fmt.Println("File doesn't Exist")
 	}else{
-		fmt.Println(file.Name()+ " opened successfully!")
+		fmt.Println(file1.Name()+ " opened successfully!")
 	}
+
+	sampleErr1 := errors.New("New error occured")
+	fmt.Println(sampleErr1)
+
+	sampleErr2 := fmt.Errorf("Error occured in %s","db connection")// error with formatting of error messages
+	fmt.Println(sampleErr2) 
+
+	file2 ,_ := os.Open("sample.text") // underscore (‘_’) operator can be used to ignore the error
+	fmt.Println(file2)
 }
